e08bc818aa03d7259e4fe52ca52a353f14b309ea
Chen Gong <ChenG_abc@outlook.com>
2024-11-27 14:41:49 -0500

    doc: add rq3.sh

---
+++ scripts/rq3.sh
@@ -0,0 +1,5 @@
+# In RQ3, we investigate the conditional and unconditional pretraining of the studied algorithms using ImageNet as the pretraining dataset.
+
+CUDA_VISIBLE_DEVICES=0 python run.py public_data.name=imagenet --epsilon=10.0 pretrain.cond=true --method DP-Kernel --data_name mnist_28 eval.mode=val --exp_description val_condition_imagenet  &
+
+CUDA_VISIBLE_DEVICES=1 python run.py public_data.name=imagenet --epsilon=10.0 pretrain.cond=true --method DP-Kernel --data_name cifar10_32 eval.mode=val --exp_description val_condition_imagenet  &
