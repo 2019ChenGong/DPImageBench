7e592b05a889fd60826f3c2b0061457024b42768
Chen Gong <ChenG_abc@outlook.com>
2024-12-25 00:29:31 -0500

    edit: installation

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ conda create -n dpimagebench python=3.9;
 conda activate dpimagebench;
 pip install torch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 --index-url https://download.pytorch.org/whl/cu121;
 conda install -y -c pytorch -c nvidia faiss-gpu=1.8.0;
-pip install requirements.txt;
+pip install -r requirements.txt;
 cd opacus; pip install -e .; cd ..
 cd models/PE/improved-diffusion; pip install -e .; cd ..; cd ..; cd ..
 cd models; gdown https://drive.google.com/uc?id=1yVTWzaSqJVDJy8CsZKtqDoBNeM6154D4; unzip pretrained_models.zip; cd ..
